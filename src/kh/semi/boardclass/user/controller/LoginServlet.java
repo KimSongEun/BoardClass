084377fec15e652612f0cdd183e36ec7aec7bac4
KyoungSook1 <Kyoungsook001@gmail.com>
2021-10-13 10:15:32 +0900

    로그인/로그아웃 기능 jsp, servlet, css 파일 수정 및 추가생성

--- src/kh/semi/boardclass/user/controller/LoginServlet.java
+++ src/kh/semi/boardclass/user/controller/LoginServlet.java
@@ -1,24 +1,14 @@
 package kh.semi.boardclass.user.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import kh.semi.boardclass.user.model.service.UserService;
-import kh.semi.boardclass.user.model.vo.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -39,60 +29,13 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("/WEB-INF/user/login.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("/login 진입");
-
-
-		response.setContentType("application/json;charset=UTF-8");
-		UserService mservice = new UserService();
-		String userId = request.getParameter("userId");
-		String userPassword = request.getParameter("userPassword");
-		System.out.println("userId: " + userId);
-		System.out.println("userPassword: " + userPassword);
-
-		PrintWriter out = response.getWriter();
-
-		Gson gob = new GsonBuilder().setPrettyPrinting().create(); // console창에 이쁘게 보이는 것
-//		Gson gob = new GsonBuilder().create();
-
-		String gobStr = "";
-
-		List<User> voList = new ArrayList<User>();
-		User user = mservice.login(userId, userPassword);
-		if (user != null) { // 로그인 성공
-			System.out.println("로그인성공");
-			HttpSession session = request.getSession();
-			session.setAttribute("user", user);
-
-			voList.add(user);
-			voList.add(user);
-
-			Map<String, Object> map2 = new HashMap<String, Object>();
-			map2.put("result", "ok");
-			map2.put("user_Name", user.getUserName());
-			map2.put("memberInfo", user);
-
-			gobStr = gob.toJson(map2);
-
-		} else { // 로그인 실패
-			System.out.println("로그인실패");
-			// 예시 1
-			Map<String, Object> map2 = new HashMap<String, Object>();
-			map2.put("result", "fail");
-			gobStr = gob.toJson(map2);
-		}
-		System.out.println("gobStr: " + gobStr);
-		out.println(gobStr);
-		out.flush();
-		out.close();
-		//request.getRequestDispatcher("/WEB-INF/user/login.jsp").forward(request, response);
-		
 	}
 
 }
