098171667b47a9ed7b3cf4e7aa1544c377db6918
KimSongEun <songeun4939@naver.com>
2021-10-20 18:29:03 +0900

    메인페이지 진입을 위한 구성, 메인페이지_광고 carousel 만들기

---
+++ src/kh/semi/boardclass/main/model/dao/MainDao.java
@@ -0,0 +1,9 @@
+package kh.semi.boardclass.main.model.dao;
+
+public class MainDao {
+
+	public MainDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/kh/semi/boardclass/main/model/service/MainService.java
@@ -0,0 +1,9 @@
+package kh.semi.boardclass.main.model.service;
+
+public class MainService {
+
+	public MainService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
