e44204a3edf90e5d5f06d8978d3aa3114c97d41a
2yunbi <dbsql10145@gmail.com>
2021-10-26 12:16:34 +0900

    자유게시판 검색 수정

--- src/kh/semi/boardclass/community/controller/SearchAllBoardUserIdServlet.java
+++ src/kh/semi/boardclass/community/controller/SearchAllBoardUserIdServlet.java
@@ -34,49 +34,33 @@ public class SearchAllBoardUserIdServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		
-		String viewcount = request.getParameter("viewcount");
-		int PAGE_SIZE;
-		if(viewcount != null) {
-			PAGE_SIZE = Integer.parseInt(viewcount);
-		} else {
-			PAGE_SIZE = 10;
-		}
-		final int PAGE_BLOCK = 5; 
-		int aCount = 0; 
-		int pageCount = 0; 
-		int startPage = 1;
-		int endPage = 1;
-		int currentPage = 1;
-		int startRnum = 1;
-		int endRnum = 1; 
-		
-		String pageNum = request.getParameter("pagenum");
-		if(pageNum !=null) { 
-				currentPage=Integer.parseInt(pageNum);
-		}
 		String type = request.getParameter("type");
 		String keyword = request.getParameter("keyword");
-		aCount = new CommunityService().getAllBoardUserIdCount(keyword);
-		pageCount = (aCount / PAGE_SIZE) + (aCount % PAGE_SIZE == 0 ? 0:1);
-		startRnum = (currentPage-1) * PAGE_SIZE + 1;  
-		endRnum = startRnum + PAGE_SIZE -1;
-		if(endRnum > aCount) endRnum = aCount;
-		
-		if (currentPage % PAGE_BLOCK == 0) {
-			startPage = (currentPage / PAGE_BLOCK -1) * PAGE_BLOCK + 1;
-		} else {
-			startPage = (currentPage / PAGE_BLOCK) * PAGE_BLOCK + 1;
-		}
-		endPage = startPage + PAGE_BLOCK -1;
-		if(endPage > pageCount) endPage = pageCount;
-		
-		ArrayList<Board> list  = new CommunityService().searchAllBoardUserId(keyword, startRnum, endRnum);
+		int totCnt = new CommunityService().getAllBoardUserIdCount(keyword);
+		String pageNum = request.getParameter("pageNum");
+		if(pageNum == null || pageNum.equals("")) {pageNum = "1";}
+		int currentPage = Integer.parseInt(pageNum);
+		int pageSize = 10, blockSize = 10;
+		int startRow = (currentPage - 1) * pageSize + 1;
+		int endRow = startRow + pageSize -1;
+		int startNum = totCnt - startRow +1;
+
+		int pageCnt = (int)Math.ceil((double)totCnt/pageSize);
+		int startPage = (int)(currentPage -1) / blockSize * blockSize + 1;
+		int endPage = startPage + blockSize - 1;
+		if( endPage > pageCnt ) endPage = pageCnt;
+
+		ArrayList<Board> list  = new CommunityService().searchAllBoardUserId(keyword, startRow, endRow);
 	
-		request.setAttribute("viewcount", PAGE_SIZE);
+		request.setAttribute("totCnt", totCnt);
+		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("startNum", startNum);
 		request.setAttribute("list", list);
+		request.setAttribute("blockSize", blockSize);
+		request.setAttribute("pageCnt", pageCnt);
 		request.setAttribute("startPage", startPage);
 		request.setAttribute("endPage", endPage);
-		request.setAttribute("pageCount", pageCount);
 		request.setAttribute("keyword", keyword);
 		request.setAttribute("type", type);
 		
